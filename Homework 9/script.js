fbf60dae12efaf3338d08a6a7fdcd5e6850fc70b
Aleksandr <fedinaleksandr23@gmail.com>
2023-07-13 14:47:23 +0300

    update

--- Homework 9/script.js
+++ Homework 9/script.js
@@ -1,4 +1,39 @@
+function getRandomColor () {
+    const letters = `0123456789ABCDEF`;
+    let  color = `#`;
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+function changeSquareColor () {
+    const squares = document.getElementsByClassName(`square`);
+
+    for (let i = 0; i < 25; i++) {
+        const square = squares[i];
+        square.style.backgroundColor = getRandomColor();
+    }
+}
+
 function generateBlocks () {
+    const container = document.getElementById(`container`);
+
+    for (let i = 0; i < 25; i++) {
+        const square = document.createElement(`div`);
+        square.classList.add(`square`);
+        square.style.backgroundColor = getRandomColor();
+        container.appendChild(square);
+    }
+}
+
+generateBlocks();
+setInterval(changeSquareColor, 1000);
+
+
+// Старий код.
+
+/* function generateBlocks () {
     const container = document.getElementById(`container`);
 
     for (let i = 0; i < 25; i++) {
@@ -40,4 +75,4 @@ function generateBlocksInterval () {
     }, 1000);
 }
 
-generateBlocksInterval();
+generateBlocksInterval(); */
