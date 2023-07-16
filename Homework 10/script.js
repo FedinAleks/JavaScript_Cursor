c4c97d01b9348ec7be4ea23b37836b7727e91af7
Aleksandr <fedinaleksandr23@gmail.com>
2023-07-16 15:50:22 +0300

    Homework 10

--- Homework 10/script.js
+++ Homework 10/script.js
@@ -1,8 +1,40 @@
-window.addEventListener(`keydown`, function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+let lastPlayedAudio = null
 
+function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
     if (!audio) return;
+    
+    if (lastPlayedAudio) {
+        lastPlayedAudio.pause();
+        lastPlayedAudio.currentTime = 0;
+        lastPlayedAudio.classList.remove(`active`);
+    };
     audio.currentTime = 0;
     audio.play();
-})
+    lastPlayedAudio = audio;
+    
+    key.classList.toggle(`playing`);
+    };
+
+function removeTransition(e) {
+    if (e.propertyName !== `transform`) return;
+    this.classList.remove(`playing`);
+}
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener(`transitionend`, removeTransition))
+window.addEventListener(`keydown`, playSound);
+
+//Кнопка зупинки звуку
+
+const stopSound = document.getElementById(`stopSound`);
+
+stopSound.addEventListener(`click`, function() {
+    if (lastPlayedAudio) {
+        lastPlayedAudio.pause();
+        lastPlayedAudio.currentTime = 0;
+    }
+    lastPlayedAudio.classList.remove(`active`);
+});
