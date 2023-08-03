684b6058924f5b749aa198ca4d1ff20f6b3ddf37
Aleksandr <fedinaleksandr23@gmail.com>
2023-08-03 15:08:52 +0300

    update

--- Homework 13/script.js
+++ Homework 13/script.js
@@ -1,3 +1,5 @@
+// Task 1
+/*
 function* createIdGenerator() {
     let id = 10;
     while (true) {
@@ -8,10 +10,8 @@ function* createIdGenerator() {
   const idGenerator = createIdGenerator();
   
 console.log(idGenerator.next().value); 
-console.log(idGenerator.next().value); 
-console.log(idGenerator.next().value);
-console.log(idGenerator.next().value);
-console.log(idGenerator.next().value);
+
+// Task 2
 
 function* newFontGenerator(initialSize) {
     let fontSize = initialSize;
@@ -33,8 +33,40 @@ console.log(fontGenerator.next("up").value);
 console.log(fontGenerator.next("up").value);
 console.log(fontGenerator.next().value);
 console.log(fontGenerator.next("down").value);
-console.log(fontGenerator.next("down").value);
-console.log(fontGenerator.next("down").value);
-console.log(fontGenerator.next().value);
-console.log(fontGenerator.next("down").value);
-console.log(fontGenerator.next("up").value);
+*/
+
+function* newFontGenerator(initialSize) {
+  let fontSize = initialSize;
+  const defaultSize = initialSize;
+
+  while (true) {
+    const change = yield fontSize;
+    if (change === "up") {
+      fontSize += 2;
+    } else if (change === "down") {
+      fontSize -= 2;
+    } else if (change === "reset") { // Обробник скидання розміру
+      fontSize = defaultSize;
+    }
+
+    // Зміна розміру шрифту елемента з id="text"
+    document.getElementById("text").style.fontSize = `${fontSize}px`;
+    document.getElementById("currentSize").innerText = `Current Size: ${fontSize}px`;
+  }
+}
+
+const fontGenerator = newFontGenerator(14);
+
+// Обробник натискання кнопки "Up"
+document.getElementById("upButton").addEventListener("click", () => {
+  fontGenerator.next("up");
+});
+
+// Обробник натискання кнопки "Down"
+document.getElementById("downButton").addEventListener("click", () => {
+  fontGenerator.next("down");
+});
+
+document.getElementById("resetButton").addEventListener("click", () => {
+  fontGenerator.next("reset");
+});
